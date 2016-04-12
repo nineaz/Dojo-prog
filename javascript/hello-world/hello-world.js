73b425933d26743e92380216a38b0d4493d4d59b
Bu7ch <dutchr@live.fr>
2016-04-12 09:41:52 +0200

    fix emprty file

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,10 +1,1 @@
 
-
-var HelloWorld = function() {};
-
-HelloWorld.prototype.hello = function(name) {
-  name = name || 'World';
-    return 'Hello, ' + name + '!';
-};
-
-module.exports = HelloWorld;
