71fa88417d20a7be3a49348221e50acba23d8583
Lunem <lydia.detienne@gmail.com>
2016-04-12 14:22:32 +0200

    exo bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,1 +1,24 @@
-
+var bob = function() {
+  this.hey = function(phrase) {
+    function isAquestion() {
+      return phrase[phrase.length - 1] === '?';
+    }
+    function isShouting() {
+      return phrase.toUpperCase() === phrase && /[A-Z]/.test(phrase);
+    }
+    function isMute() {
+      return phrase.length === 0 || phrase === '   ';
+    }
+    if (isMute()) {
+      return 'Fine. Be that way!';
+    }
+    else if (isShouting()) {
+      return 'Whoa, chill out!';
+    }
+    else if (isAquestion()) {
+      return 'Sure.';
+    }
+    return 'Whatever.';
+    };
+  };
+  module.exports = bob;
--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,19 +1,14 @@
-
-
-var HelloWorld = function() {};
-HelloWorld.prototype.hello = function (name) {
-  if (name === 'Bob' || name === 'Sally') {
-    return 'Hello, ' + name + '!';
-  } else {
-    return 'Hello, World!';
-  }
-
+var HelloWorld = function() {
+  this.hello = function(nom){
+    if (nom === '') {
+      return("Hello, World!");
+    }
+    else {
+      return("Hello, " + nom + "!" );
+    }
+  };
 };
 
-
-
-
-
-
-
+// var coucou = new HelloWorld();
+// console.log(coucou.hello("Edwin"));
 module.exports = HelloWorld;
