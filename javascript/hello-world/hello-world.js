dd294a78539ffddbeb920a9e68c30660519dd218
Bu7ch <dutchr@live.fr>
2016-04-21 17:07:52 +0200

    fix conflicts

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,1 +1,14 @@
+var HelloWorld = function() {
+  this.hello = function(nom){
+    if (nom === '') {
+      return("Hello, World!");
+    }
+    else {
+      return("Hello, " + nom + "!" );
+    }
+  };
+};
 
+// var coucou = new HelloWorld();
+// console.log(coucou.hello("Edwin"));
+module.exports = HelloWorld;
